eb5f518b2c9825b7827a91af1fd800ec5633a283
montheooo <montheooo@yahoo.fr>
2023-04-17 19:57:21 -0400

    Add project files.

---
+++ Generic/MyGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class MyGeneric<T> where T : struct
+    {
+        private Collection<T> collection;
+
+        public void addItem(T item)
+        {
+            collection.Add(item);
+        }
+
+        public T getItem(int index)
+        {
+            return collection[index];
+        }
+    }
+}
